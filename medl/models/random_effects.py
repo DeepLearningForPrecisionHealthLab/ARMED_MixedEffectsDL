b4b67f8699d695f842d973a72f8128e3f2d98aad
Kevin Nguyen <kevin3.nguyen@UTSouthwestern.edu>
2021-01-22 14:32:35 -0600

    Improvements to posterior and prior distributions for RE

--- medl/models/random_effects.py
+++ medl/models/random_effects.py
@@ -10,31 +10,60 @@ from tensorflow_probability.python.layers import util as tfp_layers_util
 from tensorflow_probability.python.distributions import kullback_leibler as kl_lib
 import numpy as np
 
-def posterior_fn(dtype, shape, name, trainable, add_variable_fn):
-    loc = add_variable_fn(name=name + '_loc',
-                        shape=shape,
-                        initializer=tf.initializers.random_uniform(minval=-1, maxval=1),
-                        dtype=dtype,
-                        trainable=trainable)
+def make_posterior_fn(loc_init_std=1, scale_init_range=(0.01, 1)):
 
-    untransformed_scale = add_variable_fn(name=name + '_untransformed_scale',
-                                        shape=[1],
-                                        initializer=tf.initializers.random_uniform(minval=0.01, maxval=1),
-                                        dtype=dtype,
-                                        trainable=trainable)
-    scale = tfu.DeferredTensor(untransformed_scale,
-                                    lambda x: (np.finfo(dtype.as_numpy_dtype).eps + tf.nn.softplus(x)))
-    dist = tfd.Normal(loc=loc, scale=scale)
-    batch_ndims = tf.size(dist.batch_shape_tensor())
-    return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
+    def _posterior_fn(dtype, shape, name, trainable, add_variable_fn):
+        loc = add_variable_fn(name=name + '_loc',
+                            shape=shape,
+                            initializer=tf.initializers.random_normal(mean=0.0, stddev=loc_init_std),
+                            dtype=dtype,
+                            trainable=trainable)
+        untransformed_scale = add_variable_fn(name=name + '_untransformed_scale',
+                                            shape=shape[1:],
+                                            initializer=tf.initializers.random_uniform(minval=scale_init_range[0], maxval=scale_init_range[1]),
+                                            dtype=dtype,
+                                            trainable=trainable)
+        scale = tfu.DeferredTensor(untransformed_scale,
+                                        lambda x: (np.finfo(dtype.as_numpy_dtype).eps + tf.nn.softplus(x)))
+        dist = tfd.Normal(loc=loc, scale=scale)
+        batch_ndims = tf.size(dist.batch_shape_tensor())
+        return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
+
+    return _posterior_fn
+
+def make_prior_fn(prior_scale=1):
+    def _prior_mvn_fn(dtype, shape, name, trainable, add_variable_fn):
+        """Creates multivariate standard `Normal` distribution.
+
+        Args:
+            dtype: Type of parameter's event.
+            shape: Python `list`-like representing the parameter's event shape.
+            name: Python `str` name prepended to any created (or existing)
+            `tf.Variable`s.
+            trainable: Python `bool` indicating all created `tf.Variable`s should be
+            added to the graph collection `GraphKeys.TRAINABLE_VARIABLES`.
+            add_variable_fn: `tf.get_variable`-like `callable` used to create (or
+            access existing) `tf.Variable`s.
+
+        Returns:
+            Multivariate standard `Normal` distribution.
+        """
+        dist = tfd.Normal(loc=tf.zeros(shape, dtype), scale=prior_scale)
+        batch_ndims = tf.size(dist.batch_shape_tensor())
+        return tfd.Independent(
+            dist, reinterpreted_batch_ndims=batch_ndims)
+    return _prior_mvn_fn    
 
 class DenseRandomEffects(tfplayers.DenseFlipout):
     def __init__(self, 
                  units,
+                 loc_init_std=1,
+                 scale_init_range=(0.01, 1),
+                 prior_scale=1,
                  activation=None,
                  activity_regularizer=None,
                  trainable=True,
-                 kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
+                #  kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
                  kernel_divergence_fn=lambda q, p, ignore: kl_lib.kl_divergence(q, p),
                  seed=None,
                  **kwargs):
@@ -53,9 +82,9 @@ class DenseRandomEffects(tfplayers.DenseFlipout):
                                                 activation=activation,
                                                 activity_regularizer=activity_regularizer,
                                                 trainable=trainable,
-                                                kernel_posterior_fn=posterior_fn,
+                                                kernel_posterior_fn=make_posterior_fn(loc_init_std, scale_init_range),
                                                 kernel_posterior_tensor_fn=(lambda d: tf.math.reduce_mean(d.sample(100), axis=0)),
-                                                kernel_prior_fn=kernel_prior_fn,
+                                                kernel_prior_fn=make_prior_fn(prior_scale),
                                                 kernel_divergence_fn=kernel_divergence_fn,
                                                 bias_posterior_fn=None,
                                                 bias_posterior_tensor_fn=None,
