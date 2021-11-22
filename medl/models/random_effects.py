0d96cca31f1119e5b734b681a0b8dbd7377c7a9a
Kevin Nguyen <kevin3.nguyen@UTSouthwestern.edu>
2021-11-22 10:47:27 -0600

    fix serialization, add gamma dist

--- medl/models/random_effects.py
+++ medl/models/random_effects.py
@@ -29,7 +29,10 @@ def make_fixed_prior_fn(prior_scale):
     def _prior_fn(kernel_size, bias_size=0, dtype=None):
         n = kernel_size + bias_size
         return tf.keras.Sequential([tpl.DistributionLambda(lambda t: 
-                                        tpd.Normal(loc=tf.zeros(n), scale=prior_scale))])
+                                        tpd.Independent(
+                                            tpd.Normal(loc=tf.zeros(n), scale=prior_scale),
+                                            reinterpreted_batch_ndims=1))
+                                    ])
     return _prior_fn
 
 def make_trainable_prior_fn(prior_scale):
@@ -80,3 +83,91 @@ class RandomEffects(tpl.DenseVariational):
         
         return outputs
 
+class NamedVariableLayer(tpl.VariableLayer):
+    def __init__(self,
+                shape,
+                dtype=None,
+                activation=None,
+                initializer='zeros',
+                regularizer=None,
+                constraint=None,
+                name=None,
+                **kwargs) -> None:
+        '''
+        Subclass of VariableLayer that simply adds the capability to name the
+        variables. This is needed to prevent name collisions when saving model
+        weights; the original VariableLayer hardcodes the variable name to
+        'constant' for every instance.
+
+        '''
+
+        super(tpl.VariableLayer, self).__init__(**kwargs)
+
+        self.activation = tf.keras.activations.get(activation)
+        self.initializer = tf.keras.initializers.get(initializer)
+        self.regularizer = tf.keras.regularizers.get(regularizer)
+        self.constraint = tf.keras.constraints.get(constraint)
+        self.shape = shape
+
+        shape = tf.get_static_value(shape)
+        if shape is None:
+            raise ValueError('Shape must be known statically.')
+        shape = np.array(shape, dtype=np.int32)
+        ndims = len(shape.shape)
+        if ndims > 1:
+            raise ValueError('Shape must be scalar or vector.')
+        shape = shape.reshape(-1)  # Ensures vector shape.
+
+        self._var = self.add_weight(
+            name,
+            shape=shape,
+            initializer=self.initializer,
+            regularizer=self.regularizer,
+            constraint=self.constraint,
+            dtype=dtype,
+            trainable=kwargs.get('trainable', True))
+        
+    def get_config(self):
+        return {'shape': self.shape}    
+    
+'''
+Gamma posterior and prior distributions. This distribution has parameters alpha (concentration) and beta (rate, or inverse scale). 
+'''
+def make_deterministic_posterior_fn():
+    def _re_posterior_fn(kernel_size, bias_size=0, dtype=None):
+        n = kernel_size + bias_size
+        # There are n variables containing the mean of each weight and n variables
+        # containing the shared s.d. for all weights
+        initializer = tf.keras.initializers.RandomUniform(minval=0.1, maxval=1.0)
+
+        return tf.keras.Sequential([NamedVariableLayer(n, dtype=dtype, initializer=initializer, constraint='non_neg', name='posterior'),
+                                    tpl.DistributionLambda(lambda t: tpd.Deterministic(loc=t, rtol=0.00001))])
+    return _re_posterior_fn
+
+def make_gamma_prior_fn():
+    def _prior_fn(kernel_size, bias_size=0, dtype=None):
+        initializer = tf.keras.initializers.RandomUniform(minval=5, maxval=10)
+        return tf.keras.Sequential([NamedVariableLayer(1, dtype=dtype, initializer=initializer, constraint='non_neg', name='gamma'),
+                                    tpl.DistributionLambda(lambda t: 
+                                        tpd.Gamma(concentration=1.0, rate=t))])
+    return _prior_fn
+
+class GammaRandomEffects(RandomEffects):
+    def __init__(self, units=1, kl_weight=0.001, l1_weight=None, name=None) -> None:
+        self.units = units
+        self.kl_weight = kl_weight
+        self.l1_weight = l1_weight
+        
+        posterior = make_deterministic_posterior_fn()
+        prior = make_gamma_prior_fn()
+                
+        super(RandomEffects, self).__init__(units, posterior, prior, use_bias=False,
+                                            kl_weight=kl_weight,
+                                            kl_use_exact=True,
+                                            name=name)
+        
+    def get_config(self):
+        return {'units': self.units, 
+                'kl_weight': self.kl_weight, 
+                'l1_weight': self.l1_weight}
+        
